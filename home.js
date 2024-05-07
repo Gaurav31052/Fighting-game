04d8fc163083fc57e91c444ca9c3d8980655fd99
Gaurav Kumar <gauravkumar31052@gmail.com>
2024-05-07 23:44:20 +0530

    added map name

--- home.js
+++ home.js
@@ -8,6 +8,24 @@ choices.forEach((e) => {
         setmap.src = userchoice;
         localStorage.setItem(1,userchoice);
         console.log(userchoice);
+        if(userchoice === "./img/background1.jpg"){
+            document.getElementById('name').innerHTML = "Bloom";
+        }
+        else if(userchoice === "./img/background2.jpg"){
+            document.getElementById('name').innerHTML = "Skyris";
+        }
+        else if(userchoice === "./img/background3.jpg"){
+            document.getElementById('name').innerHTML = "Night Hawl";
+        }
+        else if(userchoice === "./img/background4.jpg"){
+            document.getElementById('name').innerHTML = "Caves";
+        }
+        else if(userchoice === "./img/background5.jpg"){
+            document.getElementById('name').innerHTML = "Bind";
+        }
+        else if(userchoice === "./img/background6.jpg"){
+            document.getElementById('name').innerHTML = "Forest";
+        }
     })
 });
 
